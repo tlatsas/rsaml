7aaa8d081528731386a8498493061678df44beaf
Nikos Dimitrakopoulos <n.dimitrakopoulos@pamediakopes.gr>
2012-05-03 09:58:09 +0300

    Clean up empty spaces.

--- lib/rsaml/protocol/authn_request.rb
+++ lib/rsaml/protocol/authn_request.rb
@@ -14,60 +14,60 @@ module RSAML #:nodoc:
     class AuthnRequest < Request
       # Specifies the requested subject of the resulting assertion(s).
       attr_accessor :subject
-      
-      # Specifies constraints on the name identifier to be used to represent the requested subject. If omitted, 
-      # then any type of identifier supported by the identity provider for the requested subject can be used, 
+
+      # Specifies constraints on the name identifier to be used to represent the requested subject. If omitted,
+      # then any type of identifier supported by the identity provider for the requested subject can be used,
       # constrained by any relevant deployment-specific policies, with respect to privacy, for example.
       attr_accessor :name_id_policy
-      
-      # Specifies the SAML conditions the requester expects to limit the validity and/or use of the resulting 
-      # assertion(s). The responder MAY modify or supplement this set as it deems necessary. The 
-      # information in this element is used as input to the process of constructing the assertion, rather than as 
+
+      # Specifies the SAML conditions the requester expects to limit the validity and/or use of the resulting
+      # assertion(s). The responder MAY modify or supplement this set as it deems necessary. The
+      # information in this element is used as input to the process of constructing the assertion, rather than as
       # conditions on the use of the request itself.
       attr_accessor :conditions
-      
-      # Specifies the requirements, if any, that the requester places on the authentication context that applies 
-      # to the responding provider's authentication of the presenter.  
+
+      # Specifies the requirements, if any, that the requester places on the authentication context that applies
+      # to the responding provider's authentication of the presenter.
       attr_accessor :requested_authn_context
-      
-      # Specifies a set of identity providers trusted by the requester to authenticate the presenter, as well as 
-      # limitations and context related to proxying of the <Au message to subsequent identity providers by the 
+
+      # Specifies a set of identity providers trusted by the requester to authenticate the presenter, as well as
+      # limitations and context related to proxying of the <Au message to subsequent identity providers by the
       # responder.
       attr_accessor :scoping
-      
-      # A Boolean value. If "true", the identity provider MUST authenticate the presenter directly rather than 
-      # rely on a previous security context. If a value is not provided, the default is "false". However, if both 
-      # ForceAuthn and IsPassive are "true", the identity provider MUST NOT freshly authenticate the 
+
+      # A Boolean value. If "true", the identity provider MUST authenticate the presenter directly rather than
+      # rely on a previous security context. If a value is not provided, the default is "false". However, if both
+      # ForceAuthn and IsPassive are "true", the identity provider MUST NOT freshly authenticate the
       # presenter unless the constraints of IsPassive can be met.
       attr_accessor :force_authn
-      
-      # A Boolean value. If "true", the identity provider and the user agent itself MUST NOT visibly take control 
-      # of the user interface from the requester and interact with the presenter in a noticeable fashion. If a 
+
+      # A Boolean value. If "true", the identity provider and the user agent itself MUST NOT visibly take control
+      # of the user interface from the requester and interact with the presenter in a noticeable fashion. If a
       # value is not provided, the default is "false".
       attr_accessor :is_passive
-      
+
       attr_accessor :assertion_consumer_service_index
-      
+
       attr_accessor :assertion_consumer_service_url
-      
+
       # A URI reference that identifies a SAML protocol binding to be used when returning the response message.
       attr_accessor :protocol_binding
-      
-      # Indirectly identifies information associated with the requester describing the SAML attributes the 
-      # requester desires or requires to be supplied by the identity provider in the <Response> message. The 
-      # identity provider MUST have a trusted means to map the index value in the attribute to information 
+
+      # Indirectly identifies information associated with the requester describing the SAML attributes the
+      # requester desires or requires to be supplied by the identity provider in the <Response> message. The
+      # identity provider MUST have a trusted means to map the index value in the attribute to information
       # associated with the requester.
       attr_accessor :attribute_consuming_service_url
-      
-      # Specifies the human-readable name of the requester for use by the presenter's user agent or the 
+
+      # Specifies the human-readable name of the requester for use by the presenter's user agent or the
       # identity provider
       attr_accessor :provider_name
-      
+
       # Validate the authentication request.
       def validate
         raise ValidationError, "Conditions must be of type Conditions" if conditions && !conditions.is_a?(Conditions)
       end
-      
+
       # Construct an XML fragment representing the authentication request
       def to_xml(xml=Builder::XmlMarkup.new)
         attributes = {}
