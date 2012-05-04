86f7a64def5e36db3d30ba6c8ed7c3e3cc34f8ec
Nikos Dimitrakopoulos <n.dimitrakopoulos@pamediakopes.gr>
2012-05-04 16:52:11 +0300

    More correct way of producing AttributeValue xml with namespaces and xs:string type. Backwards incompatible!

---
+++ test/attribute_value_test.rb
@@ -0,0 +1,14 @@
+require File.dirname(__FILE__) + '/test_helper'
+
+class AttributeValueTest < Test::Unit::TestCase
+  context "an attribute value" do
+    setup do
+      @attribute_value = AttributeValue.new('foo')
+    end
+    context "when producing xml" do
+      should "always include xml schema namespaces and xs:string type" do
+        assert_equal '<saml:AttributeValue xmlns:xs="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:type="xs:string">foo</saml:AttributeValue>', @attribute_value.to_xml
+      end
+    end
+  end
+end
