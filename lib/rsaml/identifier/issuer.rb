80f7f57358fdd075eda3873ea3b23fcb24f1e52e
Nikos Dimitrakopoulos <n.dimitrakopoulos@pamediakopes.gr>
2012-05-17 13:26:43 +0300

    Typo.

--- lib/rsaml/identifier/issuer.rb
+++ lib/rsaml/identifier/issuer.rb
@@ -1,7 +1,7 @@
 module RSAML #:nodoc:
   module Identifier #:nodoc:
-    # provides information about the issuer of a SAML assertion or protocol message. T
-    # Requires the use of a string to carry the issuer's name  
+    # Provides information about the issuer of a SAML assertion or protocol message.
+    # Requires the use of a string to carry the issuer's name
     class Issuer < Name
       # If no Format value is provided with this element, then the value
       # urn:oasis:names:tc:SAML:2.0:nameid-format:entity is in effect
