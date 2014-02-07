9f318e1527977d39fafa6253199bb6693e8b0299
Nikos Dimitrakopoulos <n.dimitrakopoulos@pamediakopes.gr>
2014-02-07 09:48:55 +0200

    Attempt to fix #2 (ruby 2 and icon absence).

--- lib/xml_sig/canonicalization_method.rb
+++ lib/xml_sig/canonicalization_method.rb
@@ -1,5 +1,3 @@
-require 'iconv'
-
 module XmlSig #:nodoc:
   class CanonicalizationMethod
     attr_accessor :algorithm
@@ -15,9 +13,23 @@ module XmlSig #:nodoc:
   end
   
   class XMLC14NBase
+    # Fix for Iconv and Ruby 2. Taken from: https://gist.github.com/7hunderbird/5159050
+    if ::String.method_defined?(:encode)
+      # Encodes string using Ruby's _String.encode_
+      def self.iconv(to, from, string)
+        string.encode(to, from)
+      end
+    else
+      require 'iconv'
+      # Encodes string using _iconv_ library
+      def self.iconv(to, from, string)
+        Iconv.conv(to, from, string).join
+      end
+    end
+
     # Convert the content from the given charset to UTF-8.
     def convert_to_utf8(content, from)
-      Iconv.iconv('UTF-8', from, content).join
+      self.class.iconv('UTF-8', from, content)
     end
     def convert_linebreaks(content)
       content.gsub(/\r\n/, "\n").gsub(/\r/, "\n")
